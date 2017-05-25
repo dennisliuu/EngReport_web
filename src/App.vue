35b7e20fc31c0cade385adc932fd4a565963253c
dennisliuu <dennisliuu@gmail.com>
2017-05-25 11:59:04 +0800

    animate

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-  
+
     <section class="blur-container blur-7 justify-content-center align-items-center">
       <div class="blur-box">
         <div class="container-fluid d-flex justify-content-center align-items-center flex-column">
@@ -9,130 +9,264 @@
         </div>
       </div>
     </section>
-  
+
     <hello></hello>
-  
+
+    <footer class="footer-distributed">
+      <div class="footer-left">
+        <p class="footer-company-name">Copyright &copy; Wei-zen Liu</p>
+      </div>
+      <div class="footer-right">
+        <p class="footer-links">
+          <a href="https://www.facebook.com/dennis.liu.5095" target="_blank">Facebook</a> ·
+          <a href="https://www.google.com.tw/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=%E7%94%B1%E6%AF%94%E6%BF%B1%E7%B5%90%E8%A1%A3&*"
+            target="_blank">Wife</a>
+        </p>
+      </div>
+    </footer>
+
+
     <transition name="fade" mode="out-in">
-      <button id="toTop" class="md-icon-button" @click.native="backTop" v-if="scrollPosition>200">
-        <md-icon md-iconset="ion-arrow-up-b"></md-icon>
-      </button>
+      <el-button id="toTop" type="text" icon="arrow-up" @click.native="backTop" v-if="scrollPosition>200"></el-button>
     </transition>
-  
+
+    <!--<transition name="fade" mode="out-in">
+      <md-button id="toTop" class="md-icon-button" @click.native="backTop" v-if="scrollPosition>200">
+        <md-icon md-iconset="ion-arrow-up-b"></md-icon>
+      </md-button>
+    </transition>-->
+
   </div>
 </template>
 
 <script>
-import $ from 'jquery'
-import Hello from './components/Hello'
-export default {
-  name: 'app',
-  components: {
-    Hello
-  },
-  methods: {
-    backTop() {
-      $('html, body').animate({ scrollTop: 0 }, 500)
-      return false
+  import $ from 'jquery'
+  import Hello from './components/Hello'
+  export default {
+    name: 'app',
+    data() {
+      return {
+        scrollPosition: null
+      }
     },
-    updateScroll() {
-      this.scrollPosition = window.scrollY
+    components: {
+      Hello
+    },
+    methods: {
+      backTop() {
+        $('html, body').animate({ scrollTop: 0 }, 500)
+        return false
+      },
+      updateScroll() {
+        this.scrollPosition = window.scrollY
+      }
+    },
+    mounted: function () {
+      window.addEventListener('scroll', this.updateScroll)
     }
-  },
-  mounted: function () {
-    window.addEventListener('scroll', this.updateScroll)
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Quicksand', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /*margin-top: 60px;*/
-}
+  #app {
+    font-family: 'Quicksand', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /*margin-top: 60px;*/
+  }
+  /*basic*/
 
+  a {
+    text-decoration: none !important;
+    color: #fff;
+  }
+  /* totop */
 
-/*basic*/
+  #toTop {
+    /*background-color: transparent;*/
+    text-decoration: none;
+    position: fixed;
+    bottom: 20px;
+    right: 2%;
+    overflow: hidden;
+    z-index: 9999;
+    width: 32px;
+    height: 28px;
+    border: none;
+    opacity: 0.3;
+  }
 
-a {
-  text-decoration: none !important;
-  color: #fff;
-}
+  #toTop:hover {
+    text-decoration: none;
+    position: fixed;
+    bottom: 20px;
+    right: 2%;
+    overflow: hidden;
+    z-index: 9999;
+    width: 32px;
+    height: 28px;
+    border: none;
+    opacity: 1;
+  }
+  /*banner*/
 
+  section {
+    height: 70vh;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    position: relative;
+  }
 
-/*banner*/
+  .blur-container {
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+  }
 
-section {
-  height: 70vh;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  position: relative;
-}
+  .blur-container::before,
+  .blur-container::after {
+    top: 0px;
+    left: 0px;
+    content: '';
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: absolute;
+  }
 
-.blur-container {
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed;
-}
+  .blur-container .container-fluid {
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+  }
 
-.blur-container::before,
-.blur-container::after {
-  top: 0px;
-  left: 0px;
-  content: '';
-  width: 100%;
-  height: 100%;
-  display: block;
-  position: absolute;
-}
+  .blur-container .blur-box {
+    display: block;
+    position: relative;
+  }
 
-.blur-container .container-fluid {
-  z-index: 10;
-  width: 100%;
-  height: 100%;
-}
+  .blur-container .blur-box::before,
+  .blur-container .blur-box::after {
+    top: 0px;
+    left: 0px;
+    content: '';
+    width: 100%;
+    height: 100%;
+    display: block;
+    position: absolute;
+  }
 
-.blur-container .blur-box {
-  display: block;
-  position: relative;
-}
+  .blur-container.blur-7 {
+    --bg: url(https://zero-space.s3.amazonaws.com/photos/465f5d7c-bc67-47d4-b2de-b8597d716919.jpg);
+    background-image: var(--bg);
+  }
 
-.blur-container .blur-box::before,
-.blur-container .blur-box::after {
-  top: 0px;
-  left: 0px;
-  content: '';
-  width: 100%;
-  height: 100%;
-  display: block;
-  position: absolute;
-}
+  .blur-container.blur-7 .blur-box {
+    color: #fff;
+    width: 100%;
+    height: 90%;
+    max-width: 600px;
+    max-height: 400px;
+  }
 
-.blur-container.blur-7 {
-  --bg: url(https://zero-space.s3.amazonaws.com/photos/465f5d7c-bc67-47d4-b2de-b8597d716919.jpg);
-  background-image: var(--bg);
-}
+  .blur-container.blur-7 .blur-box::after {
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    opacity: 0.9;
+    background-image: var(--bg);
+    -webkit-filter: blur(6px) brightness(100%);
+    filter: blur(6px) brightness(100%);
+  }
+  /*footer*/
 
-.blur-container.blur-7 .blur-box {
-  color: #fff;
-  width: 100%;
-  height: 90%;
-  max-width: 600px;
-  max-height: 400px;
-}
+  footer {
+    bottom: 0;
+  }
 
-.blur-container.blur-7 .blur-box::after {
-  background-size: cover;
-  background-position: center;
-  background-attachment: fixed;
-  opacity: 0.9;
-  background-image: var(--bg);
-  -webkit-filter: blur(6px) brightness(100%);
-  filter: blur(6px) brightness(100%);
-}
+  @media (max-height:800px) {
+    footer {
+      position: static;
+    }
+  }
 
+  .footer-distributed {
+    background-color: rgb(238,238,238);
+    /*background-image: url('../assets/footer.svg');*/
+    background-repeat: no-repeat;
+    background-size: cover;
+    opacity: 0.8;
+    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+    box-sizing: border-box;
+    width: 100%;
+    text-align: left;
+    padding: 60px 60px;
+    overflow: hidden;
+  }
+  /* Footer left */
+
+  .footer-distributed .footer-left {
+    float: left;
+  }
+
+  .footer-distributed .footer-links {
+    color: #fff;
+    margin: 0 0 10px;
+    padding: 0;
+    position: relative;
+  }
+
+  .footer-distributed .footer-links a {
+    display: inline-block;
+    line-height: 1.8;
+    text-decoration: none !important;
+    color: #F3A3B7;
+    font-size: 14px !important;
+    font-weight: 700;
+  }
+
+  .footer-distributed .footer-links a:hover {
+    display: inline-block;
+    line-height: 1.8;
+    text-decoration: none !important;
+    color: #96C6EB;
+    font-size: 14px !important;
+    font-weight: 800;
+    -webkit-transition: color 0.4s;
+    -moz-transition: color 0.4s;
+    transition: color 0.4s;
+  }
+
+  .footer-distributed .footer-company-name {
+    color: #F3A3B7;
+    font-size: 16px;
+    font-weight: 800;
+    margin: 0;
+  }
+  /* Footer right */
+
+  .footer-distributed .footer-right {
+    float: right;
+  }
+  /* maria */
+
+  @media (max-width: 800px) {
+    .footer-distributed {
+      padding: 30px;
+      text-align: center;
+    }
+    .footer-distributed .footer-left,
+    .footer-distributed .footer-right {
+      float: none;
+      max-width: 300px;
+      margin: 0 auto;
+    }
+    .footer-distributed .footer-left {
+      margin-bottom: 20px;
+    }
+  }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,38 +7,73 @@
         <a class="btn1" href="#about" target="_parent">About</a>
       </div>
       <div class="li1">
-        <a class="btn1" href="sp-soap/soap.html" target="_parent">Info</a>
+        <a class="btn1" href="#info" target="_parent">Info</a>
       </div>
       <div class="li1">
-        <a class="btn1" href="../commitment.html">Resource</a>
-      </div>
-      <div class="li1">
-        <a class="btn1" href="sp-contact/contact.html">Contact</a>
+        <a class="btn1" href="#resource">Resource</a>
       </div>
     </div>
     <div class="hr1"></div>
     <p></p>
-  
+
     <el-row>
       <el-col :span="24">
         <div class="grid-content">
           <div class="about" id="about">
             <div class="container">
-            <div class="col-md-6">
+              <div class="col-md-6">
+                <div class="clearfix"> </div>
+              </div>
+              <div class="col-md-6">
+                <h3 class="wow fadeInLeft animated animated title" data-wow-delay=".5s">About</h3>
+                <h4 class="wow fadeInLeft animated animated text" data-wow-delay=".5s">Once upon a time, an young man have an idea, maybe there is a better way to learn English, and the journey
+                  has begun...</h4>
+              </div>
               <div class="clearfix"> </div>
             </div>
-            <div class="col-md-6">
-              <h3 class="wow fadeInLeft animated animated title" data-wow-delay=".5s">About</h3>
-              <h4 class="wow fadeInLeft animated animated text" data-wow-delay=".5s">Once upon a time, an young man have an idea, maybe there is a better way  to learn English, and the journey has begun...</h4>
-            </div>
-            <div class="clearfix"> </div>
           </div>
-          </div>  
         </div>
       </el-col>
     </el-row>
 
-
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content">
+          <div class="about" id="info">
+            <div class="container">
+              <div class="col-md-6">
+                <div class="clearfix"> </div>
+              </div>
+              <div class="col-md-6">
+                <h3 class="wow fadeInLeft animated animated title" data-wow-delay=".5s">Info</h3>
+                <h4 class="wow fadeInLeft animated animated text" data-wow-delay=".5s">Once upon a time, an young man have an idea, maybe there is a better way to learn English, and the journey
+                  has begun...</h4>
+              </div>
+              <div class="clearfix"> </div>
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content">
+          <div class="about" id="resource">
+            <div class="container">
+              <div class="col-md-6">
+                <div class="clearfix"> </div>
+              </div>
+              <div class="col-md-6">
+                <h3 class="wow fadeInLeft animated animated title" data-wow-delay=".5s">About</h3>
+                <h4 class="wow fadeInLeft animated animated text" data-wow-delay=".5s">Once upon a time, an young man have an idea, maybe there is a better way to learn English, and the journey
+                  has begun...</h4>
+              </div>
+              <div class="clearfix"> </div>
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
 
     <!--<el-button @click="visible = true">按钮</el-button>
                   <el-dialog v-model="visible" title="Hello world">
@@ -51,38 +86,39 @@
 </template>
 
 <script>
-import Firebase from 'firebase'
+  import Firebase from 'firebase'
 
-var config = {
-  apiKey: "AIzaSyDGJYONggbsDhXsEyYvdWVgcb55RTtD-Uc",
-  authDomain: "engrp-d185a.firebaseapp.com",
-  databaseURL: "https://engrp-d185a.firebaseio.com",
-  projectId: "engrp-d185a",
-  storageBucket: "engrp-d185a.appspot.com",
-  messagingSenderId: "728270283768"
-};
+  var config = {
+    apiKey: "AIzaSyDGJYONggbsDhXsEyYvdWVgcb55RTtD-Uc",
+    authDomain: "engrp-d185a.firebaseapp.com",
+    databaseURL: "https://engrp-d185a.firebaseio.com",
+    projectId: "engrp-d185a",
+    storageBucket: "engrp-d185a.appspot.com",
+    messagingSenderId: "728270283768"
+  };
 
-var firebaseApp = Firebase.initializeApp(config)
-var db = firebaseApp.database()
+  var firebaseApp = Firebase.initializeApp(config)
+  var db = firebaseApp.database()
 
-export default {
-  name: 'hello',
-  data() {
-    return {
-      visible: false
-    }
-  },
-  firebase: function () {
-    return {
-      Reports: db.ref('Reports')
-    }
-  },
-}
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        visible: false
+      }
+    },
+    firebase: function () {
+      return {
+        Reports: db.ref('Reports')
+      }
+    },
+  }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import url(animate.css);
-@import url(basic.css);
-@import url(element.css);
+  @import url(animate.css);
+  @import url(basic.css);
+  @import url(element.css);
 </style>
